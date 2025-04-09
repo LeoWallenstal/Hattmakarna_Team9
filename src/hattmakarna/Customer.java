c3e2d9ee2ae0263df0b2270a21b5490b1614fc9b
James Ellison <145016640+JSB1995@users.noreply.github.com>
2025-04-09 16:14:25 +0200

    Customer klass och CustomerRegister klass (#3)
    - Ett utkast på dessa två klasser, där Customer hanterar en specifik instans av en kund, och CustomerRegister hanterar en lista av alla kunder, med möjlighet att kunna filtrera ut, baserat på namn, telefonnummer, emailadress.

---
+++ src/hattmakarna/Customer.java
@@ -0,0 +1,201 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package hattmakarna;
+
+import java.util.*;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+
+/**
+ *
+ * @author james
+ */
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String customerID;
+    private String adress;
+    private String postalCode;
+    private String country;
+    private ArrayList<String>telephoneNumbers;
+    private ArrayList<String>emailAdresses;
+    
+    /*private ArrayList<String>nuvarandeOrder; kanske? Där ett (eller flera) orderID 
+    sparas, och kan instansieras vid behov, via ID på liknande sätt som denna klassen. */
+    
+    private final InfDB idb;
+    
+    public Customer(HashMap<String, String> customerMap, InfDB idb){
+        this.idb = idb;
+        
+        this.firstName = customerMap.get("first_name");
+        this.lastName = customerMap.get("last_name");
+        this.adress = customerMap.get("adress");
+        this.postalCode = customerMap.get("postalcode");
+        this.country = customerMap.get("country");
+        this.telephoneNumbers = initTelephoneNumbers();
+        this.emailAdresses = initEmailAdresses();
+    }
+    
+    public Customer(String customerID, InfDB idb){
+        this.idb = idb;
+        
+        HashMap<String, String> aCustomer = new HashMap<>();
+        String sqlQuery = "SELECT * FROM Customer WHERE ID = " + customerID;
+        
+        try{
+            aCustomer = idb.fetchRow(customerID);
+        }catch(InfException ex){
+            System.out.println(ex.getMessage() + ", in Customer(), Customer.java");
+        }
+        
+        this.telephoneNumbers = initTelephoneNumbers();
+        this.emailAdresses = initEmailAdresses();
+    }
+    
+    //Initialisering
+    
+    private ArrayList<String> initTelephoneNumbers(){
+        
+        String sqlQuery = "SELECT * FROM phone WHERE ID = " 
+            + customerID;
+        
+        ArrayList<HashMap<String, String>> telephoneNumberMap = new ArrayList<>();
+        ArrayList<String> telephoneNumbers = new ArrayList<>();
+        
+        
+        try{
+            telephoneNumberMap = idb.fetchRows(sqlQuery);
+            
+        }catch(InfException ex){
+            System.out.println(ex.getMessage() + " in fetchTelephoneNumbers(), Customer.java");
+        }
+        
+        for(HashMap<String, String> aTelephoneNumber : telephoneNumberMap){
+            telephoneNumbers.add(aTelephoneNumber.get("phone_number"));
+        }
+        return telephoneNumbers;
+    }
+    
+    private ArrayList<String> initEmailAdresses(){
+        
+        String sqlQuery = "SELECT * FROM mail WHERE ID = " 
+            + customerID;
+        
+        ArrayList<HashMap<String, String>> EmailAdressesMap = new ArrayList<>();
+        ArrayList<String> EmailAdresses = new ArrayList<>();
+        
+        
+        try{
+            EmailAdressesMap = idb.fetchRows(sqlQuery);
+            
+        }catch(InfException ex){
+            System.out.println(ex.getMessage() + " in fetchTelephoneNumbers(), Customer.java");
+        }
+        
+        for(HashMap<String, String> anEmailAdress : EmailAdressesMap){
+            EmailAdresses.add(anEmailAdress.get("mail"));
+        }
+        return EmailAdresses;
+    }
+    
+    //Getters
+    
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+    
+    public String getCustomerID(){
+        return customerID;
+    }
+    
+    public ArrayList<String> getEmailAdresses(){
+        return emailAdresses;
+    }
+    
+    public ArrayList<String> getTelephoneNumbers(){
+        return telephoneNumbers;
+    }
+    
+    public String getAdress(){
+        return adress;
+    }
+    
+    //Setters
+    
+    public void setFirstName(String newFirstName){
+      /*if(Validator.isLetters(newFirstName){
+            this.firstName = newFirstName; 
+        }
+       */
+    }
+    
+    public void setLastName(String newLastName){
+      /*if(Validator.isLetters(newFirstName){
+            this.firstName = newFirstName;
+        }
+       */
+    }
+    
+    public void addTelephoneNumber(String telephoneNumber){
+      /*if(Validator.isNumbers(telephoneNumber){
+            telephoneNumbers.add(telephoneNumber);
+        }        
+        */
+    }
+    
+    public void removeTelephoneNumber(int indexPos){
+        if(indexPos >= 0 && indexPos < telephoneNumbers.size()){
+            telephoneNumbers.remove(indexPos);
+        }
+    }
+    
+    public void addEmailAdress(String emailAdress){
+        /*if(Validator.isEmailAdress(emailAdress){
+            emailAdresses.add(emailAdress);
+        }        
+        */
+    }
+    
+    public void removeEmailAdress(int indexPos){
+        if(indexPos >= 0 && indexPos < emailAdresses.size()){
+            emailAdresses.remove(indexPos);
+        }
+    }
+    
+    
+    //För att kolla ett objekts 'state' vid debug!
+    public String toString(){
+        String output = "[ID]: " + customerID + "\n[Name]: " + firstName + " "
+            + lastName + "[Adress]: " + adress + ", " + postalCode + ", " 
+            + country + "\n[Telephone Numbers]: ";
+        
+        Iterator it = telephoneNumbers.iterator();
+        while(it.hasNext()){
+            output += it.next();
+            if(it.hasNext()){
+                output += ", ";
+            }
+        }
+        
+        output += "\n[Email Adresses]: ";
+        
+        it = emailAdresses.iterator();
+        while(it.hasNext()){
+            output += it.next();
+            if(it.hasNext()){
+                output += ", ";
+            }
+        }
+        return output;
+    }
+    
+}
---
+++ src/hattmakarna/CustomerRegister.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hattmakarna;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author james
+ */
+public class CustomerRegister {
+    private final InfDB idb;
+    private final ArrayList<Customer> allCustomers;
+    
+    public CustomerRegister(InfDB idb){
+        this.idb = idb;
+        allCustomers = initAllCustomers();
+    }
+    
+    private ArrayList<Customer> initAllCustomers(){
+        //<<COLUMNNAME>> och <<NAME OF TABLE>> är placeholders.
+        
+        String sqlQuery = "SELECT * FROM <<NAME OF TABLE>>";
+        
+        ArrayList<HashMap<String, String>> customerMaps = new ArrayList<>();
+        ArrayList<Customer> customerList = new ArrayList<>();
+        
+        try{
+            customerMaps = idb.fetchRows(sqlQuery);
+        }catch(InfException ex){
+            System.out.println(" in initAllCustomers(), CustomerRegistry.java");
+        }
+        
+        for(HashMap<String, String> aCustomerMap : customerMaps){
+            customerList.add(new Customer(aCustomerMap, idb));
+        }
+        
+        return customerList;
+    }
+    
+    public ArrayList<Customer> searchByEmail(String emailSearch){
+        ArrayList<Customer> searchResult = new ArrayList<>();
+        
+        for(Customer aCustomer : allCustomers){
+            for(String anEmailAdress : aCustomer.getEmailAdresses()){
+                if(anEmailAdress.startsWith(emailSearch)){
+                    searchResult.add(aCustomer);
+                }
+            }
+        }
+        return searchResult;
+    }
+
+    public ArrayList<Customer> searchByName(String nameSearch){
+        ArrayList<Customer> searchResult = new ArrayList<>();
+        
+        for(Customer aCustomer : allCustomers){
+            String fullName = aCustomer.getFirstName() + " " 
+                + aCustomer.getLastName();
+            if(fullName.startsWith(nameSearch)){
+                searchResult.add(aCustomer);
+            }
+        }
+        return searchResult;
+    }
+    
+    public ArrayList<Customer> searchByTelephone(String telephoneSearch){
+        ArrayList<Customer> searchResult = new ArrayList<>();
+        
+        for(Customer aCustomer : allCustomers){
+            for(String aTelephoneNumber : aCustomer.getTelephoneNumbers()){
+                if(aTelephoneNumber.startsWith(telephoneSearch)){
+                    searchResult.add(aCustomer);
+                }
+            }
+        }
+        return searchResult;
+    }
+    
+    public Customer getCustomer(String customerID){
+        for(Customer aCustomer : allCustomers){
+            if(customerID.equals(aCustomer.getCustomerID())){
+                return aCustomer;
+            }
+        }
+        System.out.println("getCustomer(), in CustomerRegister.java returned NULL!");
+        return null;
+    }
+            
+}
