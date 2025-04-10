29df4ef43cc8b62fce197e5b14fcd4a8769dc802
leon <leon_bynander@icloud.com>
2025-04-10 15:01:06 +0200

    Statisk InfDb tillgängligt

--- src/hattmakarna/Hattmakarna.java
+++ src/hattmakarna/Hattmakarna.java
@@ -4,17 +4,38 @@
  */
 package hattmakarna;
 
+import javax.swing.JOptionPane;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
 /**
  *
  * @author walle
  */
 public class Hattmakarna {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+	public static InfDB idb;
+	private static final String DB_PASSWORD = "dbHattAdminPW";
+	private static final String DB_USER = "dbHattAdmin";
+	private static final String DB_NAME = "hattmakaren";
+	private static final String DB_PORT = "3306";
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+
+		// Koppla till databas
+		try {
+			idb = new InfDB(DB_NAME, DB_PORT, DB_USER, DB_PASSWORD);
+
+		} catch (InfException e) {
+			JOptionPane.showMessageDialog(null, "Koppling till databasen misslyckades! :/");
+			e.printStackTrace();
+		}
+
+		// TODO code application logic here
+	}
+
 }
