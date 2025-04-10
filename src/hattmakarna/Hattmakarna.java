4a482195f28bf7e6cb532bcbe4bece2f829503f8
Edvard Sterky <eddes12@gmail.com>
2025-04-10 13:32:37 +0200

    Inlogg klar

--- src/hattmakarna/Hattmakarna.java
+++ src/hattmakarna/Hattmakarna.java
@@ -4,17 +4,27 @@
  */
 package hattmakarna;
 
+import oru.inf.InfDB;
+import oru.inf.InfException;
 /**
  *
  * @author walle
  */
 public class Hattmakarna {
 
+    private static InfDB idb;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+        idb = new InfDB("hattmakaren", "3306", "dbHattAdmin", "dbHattAdminPW");
+        new LogInWindow(idb).setVisible(true);
+        }
+        catch(InfException ex) {
+                System.out.println(ex.getMessage());
+                } 
     }
     
 }
