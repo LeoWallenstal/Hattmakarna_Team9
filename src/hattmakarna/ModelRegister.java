176cd3ac6ce5542a826fe6bc31d849fd769261f4
Pernes770 <sebastian.pernes@gmail.com>
2025-04-11 15:34:43 +0200

    T19 (#39)
    * Create ModelRegister.java
    
    * Update
    
    Update

--- src/hattmakarna/ModelRegister.java
+++ src/hattmakarna/ModelRegister.java
@@ -3,11 +3,54 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package hattmakarna;
+import oru.inf.InfDB;
+import java.util.*;
+import oru.inf.InfException;
 
 /**
  *
  * @author sebas
  */
-public class ModelRegister {
+//Klass för modellregister
+public class ModelRegister {private final InfDB idb;
+    private final ArrayList<Model> allModels;
+    public ModelRegister(InfDB idb){
+        this.idb = idb;
+        allModels = initAllModels();
+
+}
+public ArrayList<Model> getAllHats(){
+        return allModels;
+    }
+
+    public Model getModel(String modelId){
+        for(Model aModel : allModels){
+            if(aModel.getModelId().equals(modelId)){
+                return aModel;
+            }
+        }
+        System.out.println("getModel(), in ModelRegister.java returned NULL!");
+        return null;
+    }
+
+
+    private ArrayList<Model> initAllModels(){
+        String sqlQuery = "SELECT hat_id,model_id, price,name FROM hat_model";
+        ArrayList<Model> modelList = new ArrayList<>();
+        try{
+           ArrayList<HashMap<String, String>> rows = idb.fetchRows(sqlQuery);
+           for(HashMap<String, String> row : rows) {
+               String price = row.get("price");
+               String modelId = row.get("model_id");
+               String name = row.get("name");
+
+                modelList.add(new Model(idb, modelId, price, name));
+           }
+        }catch(InfException ex){
+            System.out.println(ex.getMessage());
+        }
+        return modelList;
+    }
+
     
 }
