70c3a97203173e2449d47b74f76deba5a4a89b3e
leon <98460339+leonZtiger@users.noreply.github.com>
2025-04-11 12:59:58 +0200

    Specifikation (#34)
    * Create Specification.java
    
    specifikationsklass
    
    * filter tillagt och lite mer i specification
    
    * skibidi mbape subway mcfrape
    
    * JUint 5 jar fil tillagd
    
    * Delete .project
    
    * Delete .classpath
    
    ---------
    
    Co-authored-by: Jojoelol <arrebackjoel@gmail.com>

--- src/hattmakarna/Hattmakarna.java
+++ src/hattmakarna/Hattmakarna.java
@@ -15,7 +15,7 @@ import oru.inf.InfException;
  */
 public class Hattmakarna {
 
-  public static InfDB idb;
+	public static InfDB idb;
 	private static final String DB_PASSWORD = "dbHattAdminPW";
 	private static final String DB_USER = "dbHattAdmin";
 	private static final String DB_NAME = "hattmakaren";
@@ -28,16 +28,11 @@ public class Hattmakarna {
 
 		// Koppla till databas
 		try {
-			idb = new InfDB(DB_NAME, DB_PORT, DB_USER, DB_PASSWORD);
-  new LogInWindow(idb).setVisible(true);
+			connectToDB();
+			new LogInWindow(idb).setVisible(true);
 		} catch (InfException e) {
 			JOptionPane.showMessageDialog(null, "Koppling till databasen misslyckades! :/");
 			e.printStackTrace();
 		}
-
-            CustomerRegister cr = new CustomerRegister();
-            
-            System.out.println(cr);
-            cr.getCustomer("1").delete();
 	}
 }
