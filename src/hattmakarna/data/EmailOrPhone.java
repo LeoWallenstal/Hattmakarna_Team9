73d9b261d0237c73599da7bae73ac026b5297148
James Ellison <jamesellison95@gmail.com>
2025-04-14 13:29:22 +0200

    addEmail/Phone
    litet fönster för att lägga till email/telefonnummer

---
+++ src/hattmakarna/data/EmailOrPhone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hattmakarna.data;
+
+/**
+ *
+ * @author james
+ * Enum som används i addPhoneEmailWindow.java, för att undvika fritext i
+ * parameterlistan.
+ */
+public enum EmailOrPhone{
+        EMAIL,
+        PHONE;
+    }
